a80545648af924287220531a5f5d62f79a516580
Shalah <zoungranaala@gmail.com>
2022-03-01 20:07:56 -0500

    Added more files to be used.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+// Adding dependencies
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store); // this is used for Login sessions
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,7 +21,7 @@ const sess = {
 
 app.use(session(sess));
 
-app.use(express.json());
+app.use(express.json());  // Server set up for json
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
